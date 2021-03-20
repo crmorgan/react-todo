fa85c61836d77bbf1eaac24d9dc606066d8d8723
Chris Morgan <chris-morgan@live.com>
2021-03-19 21:09:15 -0600

    Completed app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {nanoid} from 'nanoid';
+import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
 
-function App() {
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+  function onToggleCompleted(id){
+    const updatedTasks = tasks.map(task => {
+      if(task.id === id) {
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    console.log('Task completion state changed');
+    setTasks(updatedTasks);
+  }
+
+  function onDeleteTask(id) {
+    const remainingTasks = tasks.filter(t => t.id !== id);
+    console.log('Task deleted state changed');
+    setTasks(remainingTasks);
+  }
+
+  function addTask(name){
+    const newTask = {id: nanoid(), name: name, completed: false}
+    setTasks([...tasks, newTask]);
+  }
+
+  function onEditName(id, newName) {
+    const editedTasks = tasks.map(task => {
+      if(task.id === id) {
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTasks);
+  }
+
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name} 
+      isPressed={name === filter}
+      setFilter={setFilter}/>
+  ));
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map(task => (
+      <Todo 
+        key={task.id} 
+        id={task.id} 
+        name={task.name} 
+        completed={task.completed}
+        onToggleCompleted={onToggleCompleted}
+        onDeleteTask={onDeleteTask}
+        onEditName={onEditName}/>
+    ));
+
+  const tasksRemaining = tasks.filter(task => !task.completed).length;
+  const headingText = `${tasksRemaining} task${tasksRemaining !== 1 ? 's' : ''} remaining`;
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form onSubmit={addTask}/>
+      <div className="filters btn-group stack-exception">
+        {filterList}
+      </div>
+      <h2 id="list-heading">
+        {headingText}
+      </h2>
+      <ul
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+       {taskList}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {nanoid} from 'nanoid';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const DATA = [
+  { id: nanoid(), name: "Eat", completed: true },
+  { id: nanoid(), name: "Code", completed: false },
+  { id: nanoid(), name: "Sleep", completed: false }
+];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} />
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
